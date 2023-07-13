f3e6ece013b50be05632dfb8c304e28cda6990b6
miche22e28 <mbergamasco11@googlemail.com>
2023-07-13 11:01:35 +0200

    3 Deklation

--- script_01.js
+++ script_01.js
@@ -15,10 +15,28 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName;
-firstName = "Max";
-console.log(firstName);
+// let firstName;
+// firstName = "Max";
+// console.log(firstName);
 
-let familyName;
-familyName = "Mütze";
-console.log(familyName);
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+
+// console.log(firstName + " " + familyName);
+
+/* JS ist eine untypisierte Sprache! | untyped */
+let test;
+test = "Maxine";
+test = 2;
+test = false;
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test)
